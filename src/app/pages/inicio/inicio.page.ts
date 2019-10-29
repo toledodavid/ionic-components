6552a1b172684f067329071c1781e17c01c9dfb9
David Toledo <dtoledoz0207@gmail.com>
2019-10-29 17:23:02 -0600

    47-ion-card-Added

--- src/app/pages/inicio/inicio.page.ts
+++ src/app/pages/inicio/inicio.page.ts
@@ -27,6 +27,11 @@ export class InicioPage implements OnInit {
       icon: 'radio-button-on',
       name: 'Botones y router',
       redirectTo: '/botones'
+    },
+    {
+      icon: 'card',
+      name: 'Cards',
+      redirectTo: '/card'
     }
   ];
 
