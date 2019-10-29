51e09ba8d3694f5d06caf21f685e9ecca315429e
David Toledo <dtoledoz0207@gmail.com>
2019-10-28 18:26:49 -0600

    44-ion-alert-Added

--- src/app/pages/alert/alert.page.ts
+++ src/app/pages/alert/alert.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-alert',
@@ -7,9 +8,68 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AlertPage implements OnInit {
 
-  constructor() { }
+  title:string;
+
+  constructor(public alertCtrl: AlertController) { }
 
   ngOnInit() {
+  }
+
+  async presentAlert() {
+    const alert = await this.alertCtrl.create({
+      header: 'Alert',
+      subHeader: 'Subtitle',
+      message: 'This is an alert message.',
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          handler: (blah) => {
+            console.log('Cancelar');
+          }
+        },
+        {
+          text: 'Ok',
+          handler: (blah) => {
+            console.log('Boton OK');
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+  }
+
+
+  async presentAlertPromp() {
+    const alert = await this.alertCtrl.create({
+      header: 'Alert Promp',
+      inputs: [
+        {
+          name: 'name1',
+          type: 'text',
+          placeholder: 'Escribe tu nombre'
+        },
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancelar');
+          }
+        },
+        {
+          text: 'Ok',
+          handler: (data) => {
+            console.log(data.name1);
+            this.title = data.name1;
+          }
+        }
+      ]
+    });
+
+    await alert.present();
   }
 
 }
