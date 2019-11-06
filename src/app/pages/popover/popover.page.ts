257626eff4b8227309e16df9a42f11f4adf5d408
David Toledo <dtoledoz0207@gmail.com>
2019-11-06 16:35:33 -0600

    66-ion-popover-Actions-positioning

--- src/app/pages/popover/popover.page.ts
+++ src/app/pages/popover/popover.page.ts
@@ -14,12 +14,18 @@ export class PopoverPage implements OnInit {
   ngOnInit() {
   }
 
-  async mostrarPop(){
+  async mostrarPop(evento){
     const popover = await this.popoverCtrl.create({
-      component: PopinfoComponent
+      component: PopinfoComponent,
+      event: evento,
+      mode: 'ios',
+      backdropDismiss: false
     });
 
     await popover.present();
+
+    const {data} = await popover.onWillDismiss();
+    console.log('Padre: ', data); 
   }
 
 }
